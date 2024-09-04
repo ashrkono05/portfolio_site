d400e3f17af64f2066fa395504b41b485c3c5e94
Ash <tolya@anatoliyk.com>
2024-09-04 10:57:45 -0400

    inline nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,15 +18,19 @@
 </style>
 -->
 
-<nav style='padding: 20px; border-bottom: 3px solid #ccc; margin-bottom: 60px'>
-    <a href="/"><h2>
-        <i class="nes-icon coin"></i>
-        Ash Konovalchuk
-    </h2></a>
-    <a href="/robot-revo"><h2>
-        <i class="nes-icon coin"></i>
-        Robot Revolution
-    </h2></a>
+<nav class="navbar" style='padding: 20px; border-bottom: 3px solid #ccc; margin-bottom: 60px'>
+    <a href="/">
+        <h4>
+            <i class="nes-icon coin"></i>
+            Ash Konovalchuk
+        </h4>
+    </a>
+    <a href="/robot-revo">
+        <h4>
+            <i class="nes-icon coin"></i>
+            Robot Revolution
+        </h4>
+    </a>
 </nav>
 
 <slot />
@@ -34,3 +38,9 @@
 <nav style='padding: 20px; border-bottom: 3px solid #ccc; margin-bottom: 60px'>
     <p>&copy; 2024 Ash Konovalchuk</p>
 </nav>
+
+<style>
+    .navbar a {
+        display: inline-block;
+    }
+</style>
